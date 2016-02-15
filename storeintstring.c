cbba9900b11ecb124e76a5a94dfafefd933fc1ec
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-15 17:53:55 +0600

    take integer and store it as string

---
+++ storeintstring.c
@@ -0,0 +1,10 @@
+//store integer as string
+#include<stdio.h>
+
+int main(){
+    char str[1000];
+    printf("Enter integer as input\n");
+    scanf("%s",str);
+    printf("The Integer is:%s",str);
+return 0;
+}
