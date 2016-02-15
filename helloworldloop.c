d39e4c8dcc47d1183b97af667d743a8fd6fc51c2
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-15 13:00:49 +0600

    Hello world with infinite loop

---
+++ helloworldloop.c
@@ -0,0 +1,11 @@
+//hello world using loop
+//While loop will execute forever until it is terminated, to terminate press (Ctrl + C)
+#include<stdio.h>
+#define TRUE 1
+
+int main(){
+        while(TRUE){
+            printf("Hello World\n");
+        }
+return 0;
+}
