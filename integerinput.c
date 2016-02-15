f6e1a628509218b7b120540d1b99597efe15e627
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-15 17:49:22 +0600

    take integer input from user

---
+++ integerinput.c
@@ -0,0 +1,10 @@
+//take integer as input and print
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter an integer\n");
+    scanf("%d",&n);
+    printf("The Integer is: %d",n);
+return 0;
+}
