5cbd07a291e0cb6ea49b18e019de56c581d1bb2a
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-15 19:36:50 +0600

    Addition of two numbers

---
+++ addition.c
@@ -0,0 +1,11 @@
+//addition numbers from user input
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Give two number:\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum:%d",c);
+return 0;
+}
