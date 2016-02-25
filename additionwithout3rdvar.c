85857e2400cd2272b733432fd8d116b66aca1f15
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-25 11:59:25 +0600

    add two number without using third variable

---
+++ additionwithout3rdvar.c
@@ -0,0 +1,11 @@
+//addition without using third variable
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Give two number:");
+    scanf("%d%d",&a,&b);
+    a=a+b;
+    printf("Sum= %d",a);
+return 0;
+}
