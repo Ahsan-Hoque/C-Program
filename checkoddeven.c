0f04d294ba59010ccb697bcd685fd356c7747e51
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-25 13:23:17 +0600

    Check without operator

---
+++ checkoddeven.c
@@ -0,0 +1,12 @@
+// check the number without operator
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if((a/2)*2==a)
+    printf("Even Number");
+    else
+    printf("Odd");
+return 0;
+}
