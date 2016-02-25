af59a107b9ad9d41a630aa22cda92ae936b2d095
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-25 13:19:55 +0600

    check odd even with conditional operator

---
+++ oddevenconditional.c
@@ -0,0 +1,10 @@
+// check the number using conditional operator
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("enter the number\n");
+    scanf("%d",&a);
+    a%2== 0 ? printf("Even\n") : printf("Odd\n");
+return 0;
+}
