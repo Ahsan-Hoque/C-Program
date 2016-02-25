2f638f658947203960321b666f5e7603544c2a46
RailsRNS <ahsanul.hoque@rootnext.com>
2016-02-25 13:08:06 +0600

    check the number odd or even using module operator

---
+++ oddevenmodule.c
@@ -0,0 +1,14 @@
+//check the number whether odd or even using module operator
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("even number\n");
+    else
+    printf("Odd number");
+    return 0;
+}
